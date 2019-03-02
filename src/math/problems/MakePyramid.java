ef79edfbf0ad59946c60fec9e1d2ac66cbd4e9a4
tjkhan15 <tjk-wrx@hotmail.com>
2019-03-02 18:30:53 -0500

    Pyramid completed

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -14,6 +14,14 @@ public class MakePyramid {
 
         */
 
-
+        for(int  i = 0; i <6; i++) {
+            for(int j = 0; j <6-i; j++) {
+                System.out.print(" ");
+            }
+            for(int k=0; k<=i; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
         }
-  }
+    }
+}
