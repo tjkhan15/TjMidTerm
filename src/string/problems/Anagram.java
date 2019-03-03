cb148cf0cc59612b441b9da6ea7c49150ba16a43
tjkhan15 <tjk-wrx@hotmail.com>
2019-03-03 06:07:47 -0500

    Anagram Completed

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -5,8 +5,32 @@ package string.problems;
  */
 public class Anagram {
 
-    public static void main(String[] args) {
+    public void anagram (String word, String anagram) {
         //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
         //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+
+    }
+    public static void checkAnagram(String word, String anagram){
+        if(isAnagram(word, anagram) == true){
+            System.out.println(anagram + " is an anagram of " + word);
+        }else{
+            System.out.println(anagram + " is not an anagram of " + word);
+        }
+    }
+    private static boolean isAnagram(String word, String anagram){
+        if(word.length() != anagram.length()){
+            return false;
+        }
+        char[] chars = word.toCharArray();
+        for(char c : chars){
+            int index = anagram.indexOf(c);
+            if(index != -1){
+                anagram = anagram.substring(0,index) + anagram.substring(index +1, anagram.length());
+            }else{
+                return false;
+            }
+        }
+        return anagram.isEmpty();
+
     }
 }
--- src/string/problems/UnitTestingStringProblem.java
+++ src/string/problems/UnitTestingStringProblem.java
@@ -4,5 +4,11 @@ public class UnitTestingStringProblem {
     public static void main(String[] args) {
         //Apply Unit Test into all the methods in this package.
 
+        System.out.println();
+        System.out.println("This is UnitTestingStringProblem for Anagram");
+        Anagram ana = new Anagram();
+        ana.checkAnagram("CAT","ACT");
+        ana.checkAnagram("MARY","ARMY");
+
     }
 }
