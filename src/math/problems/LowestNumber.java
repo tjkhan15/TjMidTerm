aa8f4f0bfe2d796a02b9678d6632ba6f5a073f05
tjkhan15 <tjk-wrx@hotmail.com>
2019-03-03 03:07:42 -0500

    LowestNumber Completed

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -15,6 +15,15 @@ public class LowestNumber {
 		int  array[] = new int[]{211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
 
 		//find lowest number from the array
+		int lowestNumber;
+		lowestNumber = array[0];
+		for (int anArray : array)
+		{
+			if (anArray < lowestNumber){
+				lowestNumber = anArray;
+			}
+		}
+		System.out.println("The lowest value in the given array is " + lowestNumber);
 
 		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
 		List<String> lowestValue = new ArrayList<String>();
@@ -25,9 +34,9 @@ public class LowestNumber {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("Data is reading from the Table (tbl_primenumber) and displaying to the console");
+		System.out.print("Data is reading from the Table (tbl_primenumber) and displaying to the console: ");
 		for(String st:lowestValue){
-			System.out.println(st);
+			System.out.print(st+", ");
 		}
 	}
 
