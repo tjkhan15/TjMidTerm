d56b3ca1e5a561f6e098c170055404b9b2bae26d
tjkhan15 <tjk-wrx@hotmail.com>
2019-03-03 05:52:07 -0500

    UnitTestingMath

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -4,33 +4,22 @@ package math.problems;
  * Created by mrahman on 04/02/18.
  */
 public class Factorial {
+    /*
+     * Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120.
+     * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
+     *
+     */
+    public int factorial (int Number) {
 
-    public static void main(String[] args) {
-        /*
-         * Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120.
-         * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
-         *
-         */
-        int n = 5;
-        int result = 1;
-        for (int i = 1; i <= n; i++) {
-            result = result * i;
-        }
-        System.out.println("------------------------------");
-        System.out.println("Using Iteration");
-        System.out.println("The Factorial of 5! is : " + result);
-        System.out.println("Using Recursion");
-        System.out.println("The Factorial of 5! is : " + factorial(5));
-    }
+        int fact =1;
 
-
-    //        Factorial of a given number using Recursion.
-    public static int factorial(int n) {
-        if (n==0) {
-            return 1;
-        }else {
-            return n * factorial(n-1);
+        for (int i=Number;i>=1;i--)
+        {
+            fact =fact*i;
         }
 
+        System.out.println("Factorial of "+ Number+ "! = "+fact);
+        return fact ;
     }
+
 }
--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -4,5 +4,10 @@ public class UnitTestingMath {
     public static void main(String[] args) {
         //Apply Unit testing into each classes and methods in this package.
 
+        System.out.println();
+        System.out.println("This is Factorial Unit Test");
+        Factorial fact1 = new Factorial();
+        fact1.factorial(5);
+
     }
 }
