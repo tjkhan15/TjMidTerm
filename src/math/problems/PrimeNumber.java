ba7d863832d68b6329abdd199a2d38187152d6cf
tjkhan15 <tjk-wrx@hotmail.com>
2019-03-03 05:12:47 -0500

    PrimeNumber Completed

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -1,8 +1,14 @@
 package math.problems;
 
+import databases.ConnectToSqlDB;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeNumber {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
+
 		/*
 		 * Find list of Prime numbers from number 2 to 1 million.
 		 * Try the best solution as possible.Which will take less CPU life cycle.
@@ -13,6 +19,35 @@ public class PrimeNumber {
 		 *
 		 */
 
-	}
+		int i =0;
+		int num =0;
+		//Empty String
+		String  primeNumbers = "";
 
+		for (i = 1; i <= 1000000; i++) {
+			int counter=0;
+			for(num =i; num>=1; num--) {
+				if(i%num==0) {
+					counter = counter + 1;
+				}
+			}
+
+			if (counter ==2) {
+				//Appended the Prime number to the String
+				primeNumbers = primeNumbers + i + " ";
+			}
+		}
+
+		System.out.println("Prime numbers from 1 to 1000000 are :");
+		System.out.println(primeNumbers);
+	}
 }
+
+
+
+
+
+
+
+
+//============================[ DATABASE
